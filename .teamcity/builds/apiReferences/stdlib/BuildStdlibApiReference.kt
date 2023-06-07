fb7a707c1841d9f2287378feb2b132e4f928c5da
zoobestik <kb.chernenko@gmail.com>
2023-06-07 14:27:25 +0200

    fixup! chore; fix coroutines piplene build

--- .teamcity/builds/apiReferences/stdlib/BuildStdlibApiReference.kt
+++ .teamcity/builds/apiReferences/stdlib/BuildStdlibApiReference.kt
@@ -1,18 +1,30 @@
 package builds.apiReferences.stdlib
 
+import builds.apiReferences.kotlinx.coroutines.KotlinxCoroutinesBuildApiReference
 import jetbrains.buildServer.configs.kotlin.AbsoluteId
 import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.FailureAction
+import jetbrains.buildServer.configs.kotlin.ReuseBuilds
 
 object BuildStdlibApiReference : BuildType({
   name = "Stdlib Api reference"
 
   artifactRules = "latest-version.zip"
 
+  params {
+    param("%apiTemplatesBranch%", "ktl-696-dokka-stdlib")
+    param("revers.deps.*.templatesBranch", "%templatesBranch%")
+  }
+
   dependencies {
-    artifacts(AbsoluteId("Kotlin_KotlinRelease_1820_LibraryReferenceLatestDocs")) {
-      buildRule = sameChainOrLastFinished()
-      cleanDestination = true
-      artifactRules = "latest-version.zip"
+    dependency(AbsoluteId("Kotlin_KotlinRelease_1820_LibraryReferenceLatestDocs")) {
+      snapshot {
+        reuseBuilds = ReuseBuilds.NO
+        onDependencyFailure = FailureAction.FAIL_TO_START
+      }
+      artifacts {
+        artifactRules = "latest-version.zip"
+      }
     }
   }
 })
